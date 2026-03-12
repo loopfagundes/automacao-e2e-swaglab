686faf860c8743ca0c83dbe26920008af60c4044
Luciano <lof.fagundes@gmail.com>
2026-03-12 14:21:04 -0300

    feat: seleciona e validar do produto

---
+++ cypress/e2e/actions/produtos/produtosActions.js
@@ -0,0 +1,30 @@
+import produtosElements from "../../elements/produtos/produtosElements";
+
+class ProdutosActions {
+
+    acessarPaginaProdutos() {
+        cy.url().should("include", "/inventory.html");
+        produtosElements.validarOTitulo().should("contain", "Products");
+    }
+
+    selecionarProduto(nomeProduto) {
+        produtosElements.nomeProdutoLabel().contains(nomeProduto).click();
+        produtosElements.nomeProdutoLabel().should("contain", nomeProduto);
+    }
+
+    visualizarDetalhesProduto() { 
+        produtosElements.descricaoProdutoLabel().should("be.visible");
+        produtosElements.precoProdutoText().should("be.visible");    
+    }
+
+    adicionarProdutoAoCarrinho() {
+        produtosElements.adicionarProdutoAoCarrinhoButton().click();
+    }
+
+    visualizarCarrinho() {
+        produtosElements.iconeCarrinhoButton().click();
+        produtosElements.visualizarProdutoNoCarrinhoLabel().should("contain", "Sauce Labs Backpack");
+    }
+}
+
+export default new ProdutosActions();
