0df7488487bbfcd02da33fdd4bc6cd328e99eeec
Luciano <lof.fagundes@gmail.com>
2026-03-12 14:21:36 -0300

    feat: steps pdp

---
+++ cypress/e2e/step_definitions/produtos/produtosSteps.js
@@ -0,0 +1,31 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import produtosActions from "../../actions/produtos/produtosActions";
+
+let produtos;
+
+before(() => {
+    cy.fixture("produtos").then((mensagem) => {
+        produtos = mensagem;
+    });
+});
+
+Given("que estou na página de produtos", () => {
+    produtosActions.acessarPaginaProdutos();
+});
+
+When("seleciono um produto {string}", (produtoKey) => {
+    const nomeProduto = produtos[produtoKey].nome;
+    produtosActions.selecionarProduto(nomeProduto);
+});
+
+Then("visualizo os detalhes do produto", () => {
+    produtosActions.visualizarDetalhesProduto();
+});
+
+Then("clico no botão de adicionar ao carrinho", () => {
+    produtosActions.adicionarProdutoAoCarrinho();
+});
+
+Then("devo visualizar o produto no carrinho", () => {
+    produtosActions.visualizarCarrinho();
+});
