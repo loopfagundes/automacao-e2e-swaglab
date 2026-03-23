ca4e3c89024270eb3b00b880a6cb91191d3e2754
Luciano <lof.fagundes@gmail.com>
2026-03-23 16:00:43 -0300

    refactor: const para import ESM

--- cypress.config.js
+++ cypress.config.js
@@ -1,31 +1,24 @@
-const { defineConfig } = require("cypress");
-const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-const {
-  addCucumberPreprocessorPlugin,
-} = require("@badeball/cypress-cucumber-preprocessor");
-const {
-  createEsbuildPlugin,
-} = require("@badeball/cypress-cucumber-preprocessor/esbuild");
-const { allureCypress } = require("allure-cypress/reporter");
-const cypressOnFix = require("cypress-on-fix");
+import { defineConfig } from "cypress";
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import { createEsbuildPlugin } from "@badeball/cypress-cucumber-preprocessor/esbuild";
+import { allureCypress } from "allure-cypress/reporter";
+import cypressOnFix from "cypress-on-fix";
 
 async function setupNodeEvents(on, config) {
   on = cypressOnFix(on);
-
   await addCucumberPreprocessorPlugin(on, config);
-
   on(
     "file:preprocessor",
     createBundler({
       plugins: [createEsbuildPlugin(config)],
     })
   );
-
   allureCypress(on, config);
   return config;
 }
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
     specPattern: "cypress/e2e/features/**/*.feature",
     supportFile: "cypress/support/e2e.js",
